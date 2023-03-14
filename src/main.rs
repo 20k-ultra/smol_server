f7ea58f778dd5117ce921f034cf15185a3c857be
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2023-03-14 18:02:01 -0400

    init
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    env,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: smol_server <ip> <port>");
+        std::process::exit(1);
+    }
+
+    let ip = &args[1];
+    let port = &args[2];
+
+    let listener = TcpListener::bind(format!("{}:{}", ip, port)).unwrap();
+
+    println!("smol_server listening on: {}:{}", ip, port);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
